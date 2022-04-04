1542057e70baca12e1819f4c0cab7d02fdff93a5
Иванилава А.В <ivanilova_2000@mail.ru>
2022-04-04 17:14:32 +0900

    Update:server, site

--- server/index.js
+++ server/index.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose')
 const app = express()
 
 const authRouter = require('./authRouter')
-const storageRouter = require('./storageRouter')
-const prodAccRouter = require('./prodAccRouter')
+
 
 app.use(express.json())
 app.use(express.urlencoded())
@@ -18,7 +17,7 @@ const port = process.env.port||3001
 //Запуск сервера
 const start = async () => {
     try{
-        await mongoose.connect('mongodb://node:nindzya.13@cluster0-shard-00-00.fral0.mongodb.net:27017,cluster0-shard-00-01.fral0.mongodb.net:27017,cluster0-shard-00-02.fral0.mongodb.net:27017/FABRICA-CMS?ssl=true&replicaSet=atlas-1zb0kr-shard-0&authSource=admin&retryWrites=true&w=majority')
+        await mongoose.connect('mongodb://node:nindzya.13@cluster0-shard-00-00.fral0.mongodb.net:27017,cluster0-shard-00-01.fral0.mongodb.net:27017,cluster0-shard-00-02.fral0.mongodb.net:27017/SAKURA?ssl=true&replicaSet=atlas-1zb0kr-shard-0&authSource=admin&retryWrites=true&w=majority')
         app.listen(port, () => console.log(`Приложение запустилось и слушает порт ${port}!`))
     } catch(e){
         console.log(e)
@@ -33,13 +32,13 @@ const webport = process.env.port||3002
 
 //Подключение сайта к серверу
 
-  app.use(function (req,res,next) {
-      const dateebae = new Date().toLocaleString()
-    console.log('/' + req.method + ' ' + dateebae);
-    next(); });
-  app.get('/', function(req,res){
-    res.sendFile(path + 'index.html');  });
-  app.use(express.static(path));
-  app.use('/', express);
-  app.listen(webport, function () {
-    console.log(`Сайт работает и слушает порт ${webport}!`) })
+  // app.use(function (req,res,next) {
+  //     const dateebae = new Date().toLocaleString()
+  //   console.log('/' + req.method + ' ' + dateebae);
+  //   next(); });
+  // // app.get('/', function(req,res){
+  // //   res.sendFile(path + 'index.html');  });
+  // app.use(express.static(path));
+  // app.use('/', express);
+  // app.listen(webport, function () {
+  //   console.log(`Сайт работает и слушает порт ${webport}!`) })
--- site/src/components/navbar.vue
+++ site/src/components/navbar.vue
@@ -1,87 +1,101 @@
 <template lang="pug">
-div.menuGroup1 
-div.MainFrame
-button.btnMainLogo Sakura
-button.btnMain Доставка
-button.btnMain О нас
-button.btnMain Акции
-button.btnSignUp Войти
-.line
-div.menuGroup2
-button.btnMenu Роллы
-button.btnMenu Суши
-button.btnMenu Наборы
-button.btnMenu WOK
-button.btnMenu Салаты
-button.btnMenu Десерты
-button.btnMenu Добавки
-.line
-
+div.MainFrame 
+    div.menuGroup1
+        div
+            button.btnMainLogo Sakura
+        div.buttonGroup
+            button.btnMain Доставка
+            button.btnMain О нас
+            button.btnMain Акции
+            div.buttonGroup
+                span.btnMain 0
+                img(src="@/assets/cart.svg").cartStyle
+            button.btnSignUp Войти
+    div.line
+    div.menuGroup2
+        button.btnMenu Роллы
+        button.btnMenu Суши
+        button.btnMenu Наборы
+        button.btnMenu WOK
+        button.btnMenu Салаты
+        button.btnMenu Десерты
+        button.btnMenu Добавки
+    div.line
 </template>
 <script>
-export default {
-
-}
+export default {};
 </script>
 <style lang="scss" scoped>
-.line{
-margin: 20px 0px 20px 0px;
-border-bottom: 1px solid #FE785B;  
+.cartStyle{
+    width: clamp(38px, 2vw, 98px);
 }
-.btnMainLogo{
-border: none;
-background: none;
-color: #FE785B;
-font-size: 27px;
-font-family: 'Inter Regular';
-font-style: normal;
-font-weight: 600;
+.buttonGroup{
+        display: flex;
+    align-items: center;
 }
-.btnMain{
-margin: 0px 17px 0px 17px;
-border: none;
-background: none;
-color: #000000;
-font-size: 27px;
-font-family: 'Inter Regular';
-font-style: normal;
-font-weight: 600;
+.line {
+  margin: 20px 0px 20px 0px;
+  border-bottom: 1px solid #fe785b;
 }
-.btnSignUp{
-margin: 0px 17px 0px 17px;
-border: none;
-padding: 0.7vw;
-padding-left: 1vw;
-padding-right: 1vw;
-border-radius: 10px;
-background: #FE785B;
-color: white;
-font-size: 27px;
-font-family: 'Inter Regular';
-font-style: normal;
-font-weight: 600;
+.btnMainLogo {
+  border: none;
+  background: none;
+  color: #fe785b;
+  font-size: 27px;
+  font-family: "Inter Regular";
+  font-style: normal;
+  font-weight: 600;
 }
-.btnMenu{
-margin: 0px 24px 0px 24px;
-border: none;
-background: none;
-color: #000;
-font-size: 30px;
-font-family: 'Inter Regular';
-font-style: normal;
-font-weight: 600;
+.btnMain {
+  margin: 0px 17px 0px 17px;
+  border: none;
+  background: none;
+  color: #000000;
+  font-size: clamp(14px, 1vw, 98px);
+  font-family: "Inter Regular";
+  font-style: normal;
+  font-weight: 600;
 }
-.menuGroup1{
-display: flex;
-gap: 1vw
+.btnSignUp {
+  margin: 0px 17px 0px 17px;
+  border: none;
+  padding: 0.8vw;
+  padding-left: 1vw;
+  padding-right: 1vw;
+  border-radius: 10px;
+  background: #fe785b;
+  color: white;
+  font-size: clamp(14px, 1vw, 98px);
+  font-family: "Inter Regular";
+  font-style: normal;
+  font-weight: 600;
 }
-.menuGroup2{
-display: flex;
-gap: 1vw
+.btnMenu {
+  margin: 0px 1vw 0px 1vw;
+  border: none;
+  background: none;
+  color: #000;
+  font-size: clamp(18px, 1.2vw, 98px);
+  font-family: "Inter Regular";
+  font-style: normal;
+  font-weight: 600;
 }
-.MainFrame{
-display: flex;
-justify-content: space-between;
-margin: 1vw;
+.menuGroup1 {
+  display: flex;
+  gap: 1vw;
+  justify-content: space-around;
+  align-items: center;
+}
+.menuGroup2 {
+  display: flex;
+  gap: 1vw;
+  justify-content: center;
+  align-items: center;
+}
+.MainFrame {
+  display: flex;
+  justify-content: space-between;
+  margin: 1vw;
+  flex-direction: column;
 }
 </style>

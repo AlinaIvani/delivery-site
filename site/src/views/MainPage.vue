2ed8c37278665b164aa793686bf0815d6ae24607
Alina Ivanilova <78767374+AlinaIvani@users.noreply.github.com>
2022-04-21 14:06:07 +0900

    Update MainPage.vue

---
+++ site/src/views/MainPage.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+div.Menu
+    div.line
+    div.menu1
+        span Роллы
+    div.line
+</template>
+<script>
+import { vShow } from '@vue/runtime-dom';
+import {mapState} from "vuex"
+</script>
+<style lang="scss" scoped>
+.line {
+  margin: 20px 0px 20px 0px;
+  border-bottom: 1px solid #fe785b;
+}
+</style>
